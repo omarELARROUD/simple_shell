ed9f18fcb206b43a9e23b9788c93efe981eb80fb
Omar EL ARROUD <120422460+omarELARROUD@users.noreply.github.com>
2023-07-26 11:06:40 +0100

    Update _prompt.c

--- _prompt.c
+++ _prompt.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
 /**
- * prompt - prompts and reads input from user
- * @input: input read
- * @n: size of input
- * Return: nothing
+ * prompt - Prompts and reads input from the user
+ * @input: Pointer to the input read
+ * @n: Size of the input
+ *
+ * Return: Nothing
  */
 void prompt(char **input, size_t *n)
 {
-	size_t input_length;
-	ssize_t line_sum;
+    size_t input_length;
+    ssize_t line_sum;
 
-	/*write prompt*/
-	write(STDOUT_FILENO, ":) ", 3);
+    /* Write prompt */
+    write(STDOUT_FILENO, "$ ", 2);
 
-	line_sum = getline(input, n, stdin);
+    line_sum = getline(input, n, stdin);
 
-	if (line_sum == -1)
-	{
-		write(STDOUT_FILENO, "\n", 2);
-		exit(EXIT_SUCCESS);
-	}
+    if (line_sum == -1)
+    {
+        write(STDOUT_FILENO, "\n", 1);
+        exit(EXIT_SUCCESS);
+    }
 
-	/*removes "\n" char from input and null terminate it */
-	input_length = _strcspn(*input, "\n");
-	(*input)[input_length] = '\0';
+    /* Removes "\n" char from input and null terminate it */
+    input_length = _strcspn(*input, "\n");
+    (*input)[input_length] = '\0';
 
-	if (_strcmp(*input, "exit") == 0)
-	{
-		exit(EXIT_SUCCESS);
-	}
+    if (_strcmp(*input, "exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
 }
